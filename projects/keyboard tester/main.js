91e56eee4f0aa56c2801036714e42e22cbede456
Dyyynamic <52324626+Dyyynamic@users.noreply.github.com>
2019-11-30 03:05:01 +0100

    Lots of changes to the keyboard tester
    The keyboard tester now comes with 4 different layouts as well as an optional dark theme! Settings can be accessed by pressing the cogwheel icon in the top right corner.

--- projects/keyboard tester/main.js
+++ projects/keyboard tester/main.js
@@ -1,202 +1,93 @@
-window.onload = function windowOnload() {
-    const gridItem = document.getElementsByClassName("grid-item");
+var bgColorPressed = 'grey';
+var bgColorUnpressed = 'silver';
+var currentKeyboardNumber = 0;
 
-    document.body.addEventListener('keydown', function (e) { keyPress('grey', e); });
-    document.body.addEventListener('keyup', function (e) { keyPress('silver', e); });
+window.onload = function windowOnload() {
+    window.dropDownMenu = document.getElementById("dropdown-menu");
+    window.currentKeyboard = document.getElementById("custom-60");
+
+    document.body.addEventListener('keydown', function (e) { keyPress(bgColorPressed, e); });
+    document.body.addEventListener('keyup', function (e) { keyPress(bgColorUnpressed, e); });
 
     function keyPress(bgColor, e) {
-        switch (e.code) {
-            case 'Escape':
-                gridItem[0].style.background = bgColor;
-                break;
-            case 'Digit1':
-                gridItem[1].style.background = bgColor;
-                break;
-            case 'Digit2':
-                gridItem[2].style.background = bgColor;
-                break;
-            case 'Digit3':
-                gridItem[3].style.background = bgColor;
-                break;
-            case 'Digit4':
-                gridItem[4].style.background = bgColor;
-                break;
-            case 'Digit5':
-                gridItem[5].style.background = bgColor;
-                break;
-            case 'Digit6':
-                gridItem[6].style.background = bgColor;
-                break;
-            case 'Digit7':
-                gridItem[7].style.background = bgColor;
-                break;
-            case 'Digit8':
-                gridItem[8].style.background = bgColor;
-                break;
-            case 'Digit9':
-                gridItem[9].style.background = bgColor;
-                break;
-            case 'Digit0':
-                gridItem[10].style.background = bgColor;
-                break;
-            case 'Minus':
-                gridItem[11].style.background = bgColor;
-                break;
-            case 'Equal':
-                gridItem[12].style.background = bgColor;
-                break;
-            case 'Backspace':
-                gridItem[13].style.background = bgColor;
-                break;
-            case 'Tab':
-                gridItem[14].style.background = bgColor;
-                break;
-            case 'KeyQ':
-                gridItem[15].style.background = bgColor;
-                break;
-            case 'KeyW':
-                gridItem[16].style.background = bgColor;
-                break;
-            case 'KeyE':
-                gridItem[17].style.background = bgColor;
-                break;
-            case 'KeyR':
-                gridItem[18].style.background = bgColor;
-                break;
-            case 'KeyT':
-                gridItem[19].style.background = bgColor;
-                break;
-            case 'KeyY':
-                gridItem[20].style.background = bgColor;
-                break;
-            case 'KeyU':
-                gridItem[21].style.background = bgColor;
-                break;
-            case 'KeyI':
-                gridItem[22].style.background = bgColor;
-                break;
-            case 'KeyO':
-                gridItem[23].style.background = bgColor;
-                break;
-            case 'KeyP':
-                gridItem[24].style.background = bgColor;
-                break;
-            case 'BracketLeft':
-                gridItem[25].style.background = bgColor;
-                break;
-            case 'BracketRight':
-                gridItem[26].style.background = bgColor;
-                break;
-            case 'Enter':
-                gridItem[27].style.background = bgColor;
-                gridItem[41].style.background = bgColor;
-                break;
-            case 'CapsLock':
-                gridItem[28].style.background = bgColor;
-                break;
-            case 'KeyA':
-                gridItem[29].style.background = bgColor;
-                break;
-            case 'KeyS':
-                gridItem[30].style.background = bgColor;
-                break;
-            case 'KeyD':
-                gridItem[31].style.background = bgColor;
-                break;
-            case 'KeyF':
-                gridItem[32].style.background = bgColor;
-                break;
-            case 'KeyG':
-                gridItem[33].style.background = bgColor;
-                break;
-            case 'KeyH':
-                gridItem[34].style.background = bgColor;
-                break;
-            case 'KeyJ':
-                gridItem[35].style.background = bgColor;
-                break;
-            case 'KeyK':
-                gridItem[36].style.background = bgColor;
-                break;
-            case 'KeyL':
-                gridItem[37].style.background = bgColor;
-                break;
-            case 'Semicolon':
-                gridItem[38].style.background = bgColor;
-                break;
-            case 'Quote':
-                gridItem[39].style.background = bgColor;
-                break;
-            case 'Backslash':
-                gridItem[40].style.background = bgColor;
-                break;
-            case 'ShiftLeft':
-                gridItem[42].style.background = bgColor;
-                break;
-            case 'IntlBackslash':
-                gridItem[43].style.background = bgColor;
-                break;
-            case 'KeyZ':
-                gridItem[44].style.background = bgColor;
-                break;
-            case 'KeyX':
-                gridItem[45].style.background = bgColor;
-                break;
-            case 'KeyC':
-                gridItem[46].style.background = bgColor;
-                break;
-            case 'KeyV':
-                gridItem[47].style.background = bgColor;
-                break;
-            case 'KeyB':
-                gridItem[48].style.background = bgColor;
-                break;
-            case 'KeyN':
-                gridItem[49].style.background = bgColor;
-                break;
-            case 'KeyM':
-                gridItem[50].style.background = bgColor;
-                break;
-            case 'Comma':
-                gridItem[51].style.background = bgColor;
-                break;
-            case 'Period':
-                gridItem[52].style.background = bgColor;
-                break;
-            case 'ShiftRight':
-                gridItem[53].style.background = bgColor;
-                break;
-            case 'ArrowUp':
-                gridItem[54].style.background = bgColor;
-                break;
-            case 'Slash':
-                gridItem[55].style.background = bgColor;
-                break;
-            case 'ControlLeft':
-                gridItem[56].style.background = bgColor;
-                break;
-            case 'MetaLeft':
-                gridItem[57].style.background = bgColor;
-                break;
-            case 'AltLeft':
-                gridItem[58].style.background = bgColor;
-                break;
-            case 'Space':
-                gridItem[59].style.background = bgColor;
-                gridItem[60].style.background = bgColor;
-                gridItem[61].style.background = bgColor;
-                break;
-            case 'AltRight':
-                gridItem[62].style.background = bgColor;
-                break;
-            case 'ArrowLeft':
-                gridItem[64].style.background = bgColor;
-                break;
-            case 'ArrowDown':
-                gridItem[65].style.background = bgColor;
-                break;
-            case 'ArrowRight':
-                gridItem[66].style.background = bgColor;
+        if (e.code == 'Enter') {
+            document.getElementsByClassName(e.code)[(currentKeyboardNumber) * 2].style.background = bgColor;
+            document.getElementsByClassName(e.code)[(currentKeyboardNumber) * 2 + 1].style.background = bgColor;
+        } else if (e.code == 'Space') {
+            if (currentKeyboardNumber == 0) {
+                for (let i = 0; i < 4; i++) {
+                    document.getElementsByClassName(e.code)[i].style.background = bgColor;
+                }
+            } else {
+                document.getElementsByClassName(e.code)[currentKeyboardNumber + 2].style.background = bgColor;
+            }
+        } else {
+            document.getElementsByClassName(e.code)[currentKeyboardNumber].style.background = bgColor;
         }
+    }
+}
+
+let displayed = false;
+
+function dropdownMenu() {
+    if (displayed == false) {
+        dropDownMenu.style.display = 'block';
+        displayed = true;
+    } else {
+        dropDownMenu.style.display = 'none';
+        displayed = false;
+    }
+}
+
+function darkTheme() {
+    var checkBox = document.getElementById("enable-dark-theme");
+    var gridItem = document.getElementsByClassName("grid-item");
+
+    if (checkBox.checked == true) {
+        for (let i = 0; i < gridItem.length; i++) {
+            bgColorPressed = 'rgb(28,28,28)';
+            bgColorUnpressed = 'rgb(40,40,40)';
+
+            gridItem[i].style.color = 'white';
+            gridItem[i].style.background = bgColorUnpressed;
+
+            document.getElementsByTagName("BODY")[0].style.background = 'rgb(16,16,16)';
+        }
+    } else {
+        for (let i = 0; i < gridItem.length; i++) {
+            bgColorPressed = 'grey';
+            bgColorUnpressed = 'silver';
+
+            gridItem[i].style.color = 'black';
+            gridItem[i].style.background = bgColorUnpressed;
+
+            document.getElementsByTagName("BODY")[0].style.background = 'white';
+        }
+    }
+}
+
+function layout(keyboard) {
+    currentKeyboard.style.display = 'none';
+    
+    if (keyboard == 'custom-60') {
+        document.getElementById("custom-60").style.display = 'grid';
+
+        currentKeyboard = document.getElementById("custom-60");
+        currentKeyboardNumber = 0;
+    } else if (keyboard == 'standard-tkl') {
+        document.getElementById("standard-tkl").style.display = 'grid';
+
+        currentKeyboard = document.getElementById("standard-tkl");
+        currentKeyboardNumber = 1;
+    } else if (keyboard == 'standard-100') {
+        document.getElementById("standard-100").style.display = 'grid';
+
+        currentKeyboard = document.getElementById("standard-100");
+        currentKeyboardNumber = 2;
+    } else {
+        document.getElementById("chromebook").style.display = 'grid';
+
+        currentKeyboard = document.getElementById("chromebook");
+        currentKeyboardNumber = 3;
     }
 }
